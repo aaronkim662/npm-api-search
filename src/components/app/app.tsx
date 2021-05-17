bbb27d0e612aca03b173fe413ae58be3a70162d2
aaron <aaronkim662@gmail.com>
2021-05-16 21:13:37 -0400

    added selectors

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,12 @@
 import { Provider } from 'react-redux';
 import { store } from '../../state';
 import RepositoriesList from '../repositoriesList/repositoriesList';
+import './app.sass';
 
 const App = () => {
-  return <Provider store = {store}>
-    <div>
-      <h1>Search for a package</h1>
+  return <Provider store = {store} >
+    <div id = 'app-container'>
+      <h1 id ='repository-title'>Search For a Package</h1>
       <RepositoriesList />
     </div>
   </Provider>
