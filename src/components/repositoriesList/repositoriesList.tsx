661b2a2c804a62cf65fa0e524abda1baf804039b
aaron <aaronkim662@gmail.com>
2021-05-17 20:27:20 -0400

    edited dom

--- src/components/repositoriesList/repositoriesList.tsx
+++ src/components/repositoriesList/repositoriesList.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { useActions } from "../../hooks/useActions";
 import { useTypedSelector } from "../../hooks/useTypedSelector";
 import './repositoriesList.sass'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState('');
@@ -17,13 +19,22 @@ const RepositoriesList: React.FC = () => {
   };
   return <div>
     <form id = 'search-repositories-form' onSubmit = {onSubmit}>
-      <input id = 'search-repositories-input' className = 'form-element' value = {term} onChange = {e => setTerm(e.target.value)}/>
-      <button id = 'search-repositories-button' className = 'form-element'>Search</button>
+      <div id = 'search-repositories-box'>
+        <div id = 'search-repositories-input-container'>
+          <FontAwesomeIcon className = 'input-icon' icon = {faSearch}/>
+          <input id = 'search-repositories-input' className = 'form-element' placeholder = 'Search' value = {term} onChange = {e => setTerm(e.target.value)}/>
+        </div>
+        <button id = 'search-repositories-button' className = 'form-element'>Search</button>
+      </div>
       {error && <h3 id = 'error-notification'>Network Error</h3>}
       {loading && <h3 id = 'loading-spinner'>Loading...</h3>}
       {!error && !loading &&
-        data.map((name) =>
-          <div key = {name}>{name}</div>
+        data.map((repo) =>
+          <div key = {repo.name}>
+            <div>{repo.name}</div>
+            <div>{repo.description}</div>
+            <div>{repo.link}</div>
+          </div>
         )
       }
     </form>
