71788f73fc97ffa2cabfbf2b11921f01f3ada607
aaron <aaronkim662@gmail.com>
2021-05-20 19:49:47 -0400

    added comments

--- src/state/action-types/index.ts
+++ src/state/action-types/index.ts
@@ -4,8 +4,22 @@ export enum ActionType {
   SEARCH_REPOSITORIES_ERROR = 'search_repositories_error'
 }
 
+// type SearchRates = {
+//   maintenance: number;
+//   popularity: number;
+//   quality: number;
+//   searchScore: number;
+// }
+
 export type SearchState = {
   name: string;
   description: string;
   link: string;
+  // score: SearchRates;
 }
+
+// export type SelectType= {
+//   values: string[];
+//   setValues: () => void;
+
+// }
